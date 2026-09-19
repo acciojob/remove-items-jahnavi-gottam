a51bead1b8c951cf41c7704b269ff2222c9d9784
jahnavi-gottam <148139086+jahnavi-gottam@users.noreply.github.com>
2026-09-19 22:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function () {
+    colorSelect.remove(colorSelect.selectedIndex);
+});
